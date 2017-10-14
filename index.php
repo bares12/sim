14347821ae32cfd6347eff9a5eeea33e639bc993
renosz <32728131+renosz@users.noreply.github.com>
2017-10-14 21:54:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ And Modified by Farzain - zFz ( Faraaz )
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'ACCESS TOKEN ENTE'; //Your Channel Access Token
-$channelSecret = 'SECRET TOKEN ENTE';//Your Channel Secret
+$channelAccessToken = '4L2o4FtfRY0UzQe5qpC / GJpPJLgV2kIaWT7bRvsbN4Sep + kfd9UqYC2ty0LtX9WicfJ5QIbFQR9I069WeMR1OWEgja3vn7jlOSblvFkJn7xRnQsuJWXguyE8 / VWt + GtaE8DXamzhrOpaUwYO / ST6jgdB04t89 / 1O / w1cDnyilFU ='; //Your Channel Access Token
+$channelSecret = '459f 3743255ffe8c6daf46a35e705466';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -16,7 +16,8 @@ $userId 	= $client->parseEvents()[0]['source']['userId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
 $message 	= $client->parseEvents()[0]['message'];
 $profil = $client->profil($userId);
-$pesan_datang = $message['text'];
+$pesan_datang = $message['Simsimi bot v1.1'];
+$pesan_datang = $message['Owner: http://line.me/ti/p/~renooo3'];
 
 if($message['type']=='sticker')
 {	
@@ -35,7 +36,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'API KEY SIMSIMI ENTE'; //API SimSimi
+$key = 'fc7f81ca-8b0a-4836-8006-4ffec5535539'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
